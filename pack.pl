02f58921381170e3c0c037faecd5aea1c070ca36
damianoazzolini <damiazz94@gmail.com>
2024-08-31 09:00:33 +0200

    init

---
+++ pack.pl
@@ -0,0 +1,5 @@
+name(ltools).
+title('ltools - List manipulation in Prolog').
+version('1.0').
+author('Damiano Azzolini', 'damiazz94@gmail.com').
+home('https://github.com/damianoazzolini/ltools').
